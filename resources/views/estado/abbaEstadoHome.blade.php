30ed35ce92ff099ad93ad5eb5c2ba988c02dc598
Jesús Arriaga <jarriagabarron@gmail.com>
2016-01-26 23:18:50 -0600

    secciones carrusel

--- resources/views/estado/abbaEstadoHome.blade.php
+++ resources/views/estado/abbaEstadoHome.blade.php
@@ -72,7 +72,64 @@
     <div class="container">
 
 
-        <!-- Portfolio Filter -->
+        <!--
+	controlls-over		= navigation buttons over the image
+	buttons-autohide 	= navigation buttons visible on mouse hover only
+
+	data-plugin-options:
+		"singleItem": true
+		"autoPlay": true (or ms. eg: 4000)
+		"navigation": true
+		"pagination": true
+		"items": "5"
+
+	owl-carousel item paddings
+		.owl-padding-0
+		.owl-padding-3
+		.owl-padding-6
+		.owl-padding-10
+		.owl-padding-15
+		.owl-padding-20
+-->
+        <div class="text-center">
+            <div class="owl-carousel owl-padding-3 buttons-autohide controlls-over" data-plugin-options='{"singleItem": false, "items": "5", "autoPlay": true, "navigation": true, "pagination": false}'>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/4-min.jpg" alt="">
+                    <h5>Seccion 1</h5>
+                </a>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/6-min.jpg" alt="">
+                    <h5>Seccion 2</h5>
+                </a>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/19-min.jpg" alt="">
+                    <h5>Seccion 3</h5>
+                </a>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/22-min.jpg" alt="">
+                    <h5>Seccion 4</h5>
+                </a>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/3-min.jpg" alt="">
+                    <h5>Seccion 5</h5>
+                </a>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/17-min.jpg" alt="">
+                    <h5>Seccion 6</h5>
+                </a>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/18-min.jpg" alt="">
+                    <h5>Seccion 7</h5>
+                </a>
+                <a class="img-hover" href="#">
+                    <img class="img-responsive" src="assets/images/demo/451x300/20-min.jpg" alt="">
+                    <h5>Seccion 8</h5>
+                </a>
+            </div>
+        </div>
+
+
+        <!-- Portfolio Filter
         <div class="text-center">
             <ul class="nav nav-pills mix-filter">
                 <li data-filter="all" class="filter active"><a href="#">All</a></li>
@@ -82,8 +139,11 @@
             </ul>
         </div>
         <!-- /Portfolio Filter -->
+        <!-- H3 -->
+        <div class="heading-title heading-dotted text-center">
+            <h3>Seccion <span>Servicios Multiples</span></h3>
+        </div>
 
-        <div class="divider divider-dotted"><!-- divider --></div>
 
         <div class="row">
 
